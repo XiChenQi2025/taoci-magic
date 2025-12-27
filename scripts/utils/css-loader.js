49ea719368c3440070a56571c3392b9efd00c16c
XiChenQi2025 <y1519201066@gmail.com>
2025-12-27 14:57:39 +0800

    Add files via upload

--- scripts/utils/css-loader.js
+++ scripts/utils/css-loader.js
@@ -1,1 +1,41 @@
+// CSS加载工具
+class CSSLoader {
+    constructor() {
+        this.loadedCSS = new Map();
+    }
+    
+    // 加载CSS文件
+    load(url, id) {
+        if (this.loadedCSS.has(url)) {
+            return Promise.resolve();
+        }
+        
+        return new Promise((resolve, reject) => {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = url;
+            link.id = `css-${id}`;
+            
+            link.onload = () => {
+                this.loadedCSS.set(url, true);
+                resolve();
+            };
+            
+            link.onerror = reject;
+            
+            document.head.appendChild(link);
+        });
+    }
+    
+    // 卸载CSS文件
+    unload(id) {
+        const link = document.getElementById(`css-${id}`);
+        if (link) {
+            link.remove();
+            this.loadedCSS.delete(link.href);
+        }
+    }
+}
 
+// 导出全局实例
+export const cssLoader = new CSSLoader();
